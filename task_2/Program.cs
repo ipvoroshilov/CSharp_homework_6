3954f1c91cf2ca6fbc7612d37a2513d3a6b06103
Artur Voroshilov <ipvoroshilov@ya.ru>
2022-09-07 18:16:08 +0300

    Completed.Task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// ## Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+string FindCrossCoordinates (double b1, double k1, double b2, double k2)
+{
+    double x = (b1-b2)/(k2-k1);
+    double y = (k2*b1-k1*b2)/(k2-k1);
+    return $"Координаты точки пересечения: {x} {y}";
+}
+Console.WriteLine("Введите значение b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+try
+{
+    Console.WriteLine(FindCrossCoordinates(b1, k1, b2, k2));
+}
+catch
+{
+    Console.WriteLine("error. Not number");
+}
